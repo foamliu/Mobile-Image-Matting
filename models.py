454a25ae9e06009adc2f63ad1a796db78f445546
Yang Liu <liuyang12@focusmedia.cn>
2019-10-17 14:33:12 +0800

    update

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from torch import nn
 from torchsummary import summary
+from torchvision.models.segmentation import deeplabv3_resnet101
 
 from config import device
 
@@ -7,8 +8,10 @@ from config import device
 class DIMModel(nn.Module):
     def __init__(self):
         super(DIMModel, self).__init__()
+        self.model = deeplabv3_resnet101(pretrained=True, num_classes=256)
 
     def forward(self, x):
+        x = self.model(x)
         return x
 
 
