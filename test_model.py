3ab3f95dfe67db2c81d84df3ebc4680e857dddc3
Yang Liu <liuyang12@focusmedia.cn>
2019-11-11 15:32:16 +0800

    update

---
+++ test_model.py
@@ -0,0 +1,10 @@
+import torch
+
+from models.deeplab import DeepLab
+
+if __name__ == "__main__":
+    model = DeepLab(backbone='mobilenet', output_stride=16)
+    model.eval()
+    input = torch.rand(1, 3, 513, 513)
+    output = model(input)
+    print(output.size())
