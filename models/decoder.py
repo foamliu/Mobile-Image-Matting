3634dce8c18487a39e12ba9cc6acab3804549d83
Yang Liu <liuyang12@focusmedia.cn>
2019-11-11 15:27:11 +0800

    update

--- models/decoder.py
+++ models/decoder.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from models.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
+from .sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
 
 
 class Decoder(nn.Module):
