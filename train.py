e47233e04d409fe640de093c84a490cc5e2b944a
Yang Liu <liuyang12@focusmedia.cn>
2019-11-20 16:04:54 +0800

    update

--- train.py
+++ train.py
@@ -28,10 +28,7 @@ def train_net(args):
 
         model = nn.DataParallel(model)
 
-        if args.optimizer == 'sgd':
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.mom, nesterov=True)
-        else:
-            optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        optimizer = torch.optim.LBFGS(model.parameters())
 
     else:
         checkpoint = torch.load(checkpoint)
